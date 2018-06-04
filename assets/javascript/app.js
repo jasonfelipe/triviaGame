b00e836d50d63d5007e1c98361407a26adc06522
Legend <jasonrfelipe@gmail.com>
2018-06-04 13:06:35 -0700

    added js for answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,7 +32,9 @@ var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 
-var timeleft = 20;
+var timeLeft = 20;
+var countDown;
+var timeRunning = false;
 
 //-----------Questions, Answers, And Wrong Arrays------------
 var qandA = [
@@ -45,6 +47,9 @@ var qandA = [
     {question: "What country did the United States annex in 2076?", answer: "Canada"},
     {question: "What country invaded Alaska in 2066?", answer:"China"}
 ];
+
+console.log(qandA.map(function(a) {return a.answer;}))
+console.log(qandA[0].answer)
 
 var wrong = [
     {countries: ["Africa", "Russia", "Australia", "Britain", "France", "Japan"]},
@@ -69,7 +74,7 @@ var answerArray = [];
 
 
 //OK so this takes the Wrong Years array and takes 3 random things and puts them into our Answers Array!
-for(var i = 0; i < 3; i++) {
+for(var i = 0; i < 4; i++) {
     var idx = Math.floor(Math.random() * wrong[1].years.length);
     answerArray.push(wrong[1].years[idx]);
     wrong[1].years.splice(idx, 1);
@@ -106,27 +111,39 @@ shuffledAnswers = shuffle(answerArray);
 console.log ("This is our shuffled array:" + shuffledAnswers)
 
 
-//Game instructions... Should disappear after a start button is pressed.
+
+
 $('#instructions').html("HELLO SPECIAL PERSON! <br> This is a test to see if you are suitable to be a Vault Dwellers! <br> This test is timed so BE CAREFUL! <br> DON'T PANIC! <br><br> CLICK START TO BEGIN!")
 
 
 //-------------Starting-------------------
-$('#button').click(function gamestart(){
+$('#button').on("click", function gamestart(){
 
-    $('#button').html("Reset")
+    $('#button').hide();
     $('#instructions').html("You have: 20 seconds to answer the question!")
-    var timeleft = 20
     clearInterval(countDown);
+    timeLeft = 20;
+
 //-----Timer----------
+    countDown = setInterval(decrement, 1000);
 
 
-var countDown = setInterval(function(){
-    timeleft--;
-    if(timeleft <= 0)
-        clearInterval(countDown);
-        $('#instructions').html("You have: " + timeleft + " seconds to answer the question!")
-},1000);
+function decrement (){
+    timeLeft --;
+    $('#instructions').html("You have: " + timeLeft + " seconds to answer the question!")
+    if (timeLeft === 0){
+            $('#instructions').html("YOU LOSE!")
+            $('#button').show(); //Delete this once we get the questions all done.
+            incorrect++;
+            stop();
+        }
+}
 
+
+function stop () {
+    clearInterval(countDown);
+}
+   
 
     //Question
     $('#questions').html(qandA[0].question);
@@ -137,6 +154,66 @@ var countDown = setInterval(function(){
     $('#answer3').html(shuffledAnswers[2]);
     $('#answer4').html(shuffledAnswers[1]);
     $('#answer5').html(shuffledAnswers[0]);
+
+
+
+
+
+
+//----------------------
+    $('#answer1').on('click', function (){
+        if (shuffledAnswers[4] === qandA[0].answer){
+            alert("Placeholder correct!")
+        }
+        else {
+            alert("placeholder wrong")
+        }
+    });
+//----------------------
+    $('#answer2').on('click', function (){
+        if (shuffledAnswers[3] === qandA[0].answer){
+            alert("Placeholder correct!")
+        }
+        else {
+            alert("placeholder wrong")
+        }
+    });
+//----------------------
+    $('#answer3').on('click', function (){
+        if (shuffledAnswers[2] === qandA[0].answer){
+            alert("Placeholder correct!")
+        }
+        else {
+            alert("placeholder wrong")
+        }
+    });
+//----------------------
+    $('#answer4').on('click', function (){
+        if (shuffledAnswers[1] === qandA[0].answer){
+            alert("Placeholder correct!")
+        }
+        else {
+            alert("placeholder wrong")
+        }
+//----------------------
+    $('#answer5').on('click', function (){
+        if (shuffledAnswers[0] === qandA[0].answer){
+            alert("Placeholder correct!")
+        }
+        else {
+            alert("placeholder wrong")
+        }
+    });
+
+});
+
+//Things to do now:
+//Get clicked answer to be correct/wrong
+//set timer to next question
+//make score screen at the end
+//resets the game at the end.
+
+
 
 
 });
